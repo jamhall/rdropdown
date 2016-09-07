d0a4e9fe4781d761eb1ccedaf05aa6819d9d22ea
Jamie Hall <jstevenhall@gmail.com>
2016-09-07 16:35:00 +0200

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -10,8 +10,15 @@ export function findIndexForOption(options, option) {
 }
 
 export function optionExists(options, option) {
+    const deepEqual = (x,y) => {
+      return (x && y && typeof x === 'object' && typeof y === 'object') ?
+        (Object.keys(x).length === Object.keys(y).length) &&
+          Object.keys(x).reduce(function(isEqual, key) {
+            return isEqual && deepEqual(x[key], y[key]);
+          }, true) : (x === y);
+    }
     for(let o of options) {
-      if (o === option) {
+      if (deepEqual(o, option)) {
         return true;
       }
     }
